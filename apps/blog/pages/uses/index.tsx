b8bde2583538d6c02deb83c5a28e9228baf4d978
Otis Sutton <wright.a.otis@gmail.com>
2022-05-08 11:08:59 +1200

    feat(blog): flesh out users page

--- apps/blog/pages/uses/index.tsx
+++ apps/blog/pages/uses/index.tsx
@@ -1,8 +1,36 @@
+import ReactMarkdown from "react-markdown";
+
+const content = `
+Description...
+
+### Editor and 
+
+- VScode
+
+### Terminal
+
+- Microsoft Terminal
+
+### Periphials 
+
+- KVM
+- Mic
+- Camera
+- Keyboard
+- Mouse
+`;
+
 type UsesSignature = () => JSX.Element;
 const Uses: UsesSignature = () => {
   return (
-    <div>
-      <h1>Uses</h1>
+    <div className="w-full max-w-screen-md p-4">
+      <h1 className="mb-6 text-5xl font-bold font-heading text-slate-700 dark:text-slate-200">
+        Uses
+      </h1>
+      <ReactMarkdown
+        children={content}
+        className="max-w-none mb-4 prose dark:prose-dark"
+      ></ReactMarkdown>
     </div>
   );
 };
