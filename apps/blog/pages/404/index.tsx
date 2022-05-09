9c0abe8f23d2b5974b0baa23993d5be437fad851
Otis Sutton <wright.a.otis@gmail.com>
2022-05-09 17:31:50 +1200

    feat(blog): add new 404 page

---
+++ apps/blog/pages/404/index.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import Pug from '../../assets/images/pug.jpg';
+
+type NotFoundSignature = () => JSX.Element;
+const NotFound: NotFoundSignature = () => {
+  return (
+    <div className="w-full max-w-screen-md p-4">
+      <h1 className="mb-6 text-5xl font-bold font-heading text-slate-700 dark:text-slate-200">
+        Oops! Something went wrong.
+      </h1>
+      <p className="leading-loose font-body text-slate-700 dark:text-slate-200">
+        We can't find the page you are looking for, if you got here by accident
+        please click{" "}
+        <Link href="/">
+          <a className="text-pink-500 dark:text-green-300">here</a>
+        </Link>{" "}
+        to head home.
+      </p>
+      <p className="mb-4 leading-loose font-body text-slate-700 dark:text-slate-200">
+        If you did mean to get here, please enjoy this picture of a pug.
+      </p>
+      <Image src={Pug} layout="responsive" />
+    </div>
+  );
+};
+
+export default NotFound;
--- apps/blog/pages/uses/index.tsx
+++ apps/blog/pages/uses/index.tsx
@@ -1,23 +1,39 @@
 import ReactMarkdown from "react-markdown";
 
 const content = `
-Description...
+This page is heavily inspired by [Wes Bos /uses](https://wesbos.com/uses) and is intended to answer any questions about my setup.
 
-### Editor and 
+### Editor 📜
 
-- VScode
+I use [VS Code](https://code.visualstudio.com/) with the following extensions:
 
-### Terminal
+- [ENV](https://github.com/IronGeek/vscode-env)
+- [GitLens](https://github.com/eamodio/vscode-gitlens)
+- [ColdCode](https://github.com/ericliu-el/coldcode)
+- [Todo Tree](https://github.com/Gruntfuggly/todo-tree)
+- [Github Theme](https://github.com/primer/github-vscode-theme)
+- [Indent Rainbow](https://github.com/oderwat/vscode-indent-rainbow)
+- [Light Dark Toggle](https://github.com/panzerstadt/vscode-light-dark-toggle-ext)
+- [Prettier - Minimalist plugin](https://github.com/duailibe/vscode-miniprettier)
 
-- Microsoft Terminal
+### Terminal 🐢
 
-### Periphials 
+I tend to use [Microsoft Terminal](https://www.microsoft.com/en-us/p/windows-terminal/9n0dx20hk701) with [WSL 2](https://docs.microsoft.com/en-us/windows/wsl/install) on Windows, [Kitty](https://sw.kovidgoyal.net/kitty/) on Mac and Linux.
+
+### Other 🍀
+
+Here is a list of other applications I use to assist in development or development related needs.
+
+- [Netlify](https://www.netlify.com/)
+- [Postman](https://www.postman.com/)
+
+### Periphials 🐭
 
 - KVM
 - Mic
+- Mouse
 - Camera
 - Keyboard
-- Mouse
 `;
 
 type UsesSignature = () => JSX.Element;
