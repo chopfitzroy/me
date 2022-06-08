61416416b2dc7ca1bfc8f5d5966da2a918693a81
Otis Sutton <wright.a.otis@gmail.com>
2022-06-08 20:27:59 +1200

    fix(blog): import fonts first

--- apps/blog/pages/_app.tsx
+++ apps/blog/pages/_app.tsx
@@ -1,13 +1,14 @@
+import "../assets/styles/google-font-family.css"
+import "../assets/styles/shiki-twoslash.css"
+import "../assets/styles/main.css";
+
+import { FC } from "react";
+import { AppProps } from "next/app";
 import { Footer } from "../components/Footer";
 import { NavigationBar } from "../components/NavigationBar";
 import { ContentWrapper } from "../components/ContentWrapper";
 import { LightDarkWrapper } from "../components/LightDarkWrapper";
 import { PreferencesProvider } from "../context/Preferences/PreferencesContext";
-
-import "../assets/styles/shiki-twoslash.css"
-import "../assets/styles/main.css";
-import { AppProps } from "next/app";
-import { FC } from "react";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
